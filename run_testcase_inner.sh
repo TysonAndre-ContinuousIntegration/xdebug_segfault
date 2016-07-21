64e93b9972bf211b2cebc8022fcc4e1082f04a24
Tyson Andre <tandre@ifwe.co>
2016-07-21 12:04:29 -0700

    Print php version and php-config with test case output.

--- run_testcase_inner.sh
+++ run_testcase_inner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xeu
-VERSION=${VERSION:-7.0.8}
-exec sudo docker run -v $PWD/tests:/tests xdebug_segfault:$VERSION php --php-ini tests/php.ini tests/test5.php
+VERSION=${VERSION:-7.0.9}
+exec sudo docker run -v $PWD/tests:/tests xdebug_segfault:$VERSION sh -c 'php --php-ini tests/php.ini --version; php-config; exec php --php-ini tests/php.ini tests/test5.php'
